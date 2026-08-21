3993caf0d1d5250c39886609fc5140591eb8e220
Chala-prog <sanyiihundaol@gmail.com>
2026-08-21 17:43:55 +0300

    Create printWebhook.ts

---
+++ src/printWebhook.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from "express";
+
+export function handlePrintWebhook(req: Request, res: Response) {
+  const { jobId, status } = req.body;
+
+  if (status === "confirmed") {
+    return res.send("Confirmation processed");
+  } else {
+    return res.send("Duplicate confirmation ignored");
+  }
+}
